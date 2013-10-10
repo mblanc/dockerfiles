79ee5c2feda0ad735b3443a494b6180db0c3b6e6
Matthieu Blanc <blanc.matthieu@gmail.com>
2013-10-10 12:40:47 +0200

    fix deploy.sh

--- cdh/deploy.sh
+++ cdh/deploy.sh
@@ -2,5 +2,5 @@
 
 NAMENODE=$(sudo docker run -d mblanc/cdh /usr/sbin/sshd -D)
 sleep 10
-NAMENODE_IP=$(docker inspect $NAMENODE | grep IPAddress | cut -d '"' -f 4)
-echo 'ip $NAMENODE_IP'
+NAMENODE_IP=$(sudo docker inspect $NAMENODE | grep IPAddress | cut -d '"' -f 4)
+echo ip $NAMENODE_IP
