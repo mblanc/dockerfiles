d7f244a90854653dc464919f1f5ff92232f5dc1d
Matthieu Blanc <blanc.matthieu@gmail.com>
2013-11-01 12:39:32 +0100

    wip

--- cdh-centos/deploy.sh
+++ cdh-centos/deploy.sh
@@ -5,7 +5,7 @@ echo $NAMENODE
 sleep 5
 NAMENODE_IP=$(sudo docker inspect $NAMENODE | grep IPAddress | cut -d '"' -f 4)
 echo ip $NAMENODE_IP
-pipework br1 $NAMENODE 192.168.1.2/24
+pipework br1 $NAMENODE 192.168.1.1/24
 
 
 DATANODE1=$(sudo docker run -privileged -d mblanc/centos-cdh)
@@ -13,11 +13,11 @@ echo $DATANODE1
 sleep 5
 DATANODE1_IP=$(sudo docker inspect $DATANODE1 | grep IPAddress | cut -d '"' -f 4)
 echo ip $DATANODE1_IP
-pipework br1 $DATANODE1 192.168.1.3/24
+pipework br1 $DATANODE1 192.168.1.2/24
 
 DATANODE2=$(sudo docker run -privileged -d mblanc/centos-cdh)
 echo $DATANODE2
 sleep 5
 DATANODE2_IP=$(sudo docker inspect $DATANODE2 | grep IPAddress | cut -d '"' -f 4)
 echo ip $DATANODE2_IP
-pipework br1 $DATANODE2 192.168.1.4/24
+pipework br1 $DATANODE2 192.168.1.3/24
